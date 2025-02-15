eff91a67a3a252d0f2aefa6dc4dd2fd9dfc18154
Joao Lopes <joao.lopes@showpad.com>
2025-02-15 06:43:42 +0000

    trying blob to see if treeshaking works better

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
