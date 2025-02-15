a213b8a836a8e275bf41b3d0ab76b8ba7ef3b97a
Joao Lopes <joao.lopes@showpad.com>
2025-02-15 06:27:04 +0000

    include layouts folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
