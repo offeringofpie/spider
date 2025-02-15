e0fa11a5e8885f7a21f54bc728959ab1a8f9633e
Joao Lopes <joao.lopes@showpad.com>
2025-02-15 06:23:22 +0000

    readding config file because tree-shaking

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+};
