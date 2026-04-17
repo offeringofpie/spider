502f71d7d680b3acb5964d442b4fcd674da352a0
Joao Lopes <joao.lopes@showpad.com>
2026-04-17 16:16:51 +0100

    adding IndexNow support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import indexnow from 'astro-indexnow';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react()
+        indexnow({
+      key: process.env.INDEXNOW_KEY,
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
