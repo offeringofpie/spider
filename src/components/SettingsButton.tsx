70526fab64c62f47dd62986559b1b39405ed59da
offeringofpie <mail@jlopes.eu>
2026-06-18 09:28:31 +0100

    improve settings logic

--- src/components/SettingsButton.tsx
+++ src/components/SettingsButton.tsx
@@ -11,43 +11,40 @@ const SettingsButton = ({ variant = 'header', tabIndex = 0 }: Props) => {
   const toggleSettings = () => {
     setState({
       showSettings: !state.showSettings,
-      ...(!state.showSettings ? { showTranslateBar: false } : {}),
+      ...(!state.showSettings && { showTranslateBar: false }),
     });
   };
 
-  return (
-    <>
-      {variant === 'fab' ? (
-        <button
-          onClick={toggleSettings}
-          tabIndex={tabIndex}
-          aria-label="Settings"
-          className={`btn text-base-content hover:bg-base-200 border-none rounded-full flex items-center gap-2 pl-5 pr-2 h-14 shadow-sm`}
+  return variant === 'fab' ? (
+    <button
+      onClick={toggleSettings}
+      tabIndex={tabIndex}
+      aria-label="Settings"
+      className="bg-base-300/50 backdrop-blur-xs border border-base-200 text-base-content hover:bg-base-300/70 rounded-full flex items-center gap-2 pl-5 pr-2 h-14 transition-all"
+    >
+      <span className="text-sm font-medium">Settings</span>
+      <div className="bg-info/10 text-info rounded-full p-2">
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          viewBox="0 0 512.002 512.002"
         >
-          <span className="text-sm font-medium">Settings</span>
-          <div className="bg-info/10 text-info rounded-full p-2">
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5"
-              viewBox="0 0 512.002 512.002"
-            >
-              <use href="#spiderweb" />
-            </svg>
-          </div>
-        </button>
-      ) : (
-        <button
-          onClick={toggleSettings}
-          className={`text-primary-focus cursor-pointer hover:text-primary h-full relative transition-all duration-300 ${state.showSettings ? 'text-primary rotate-180 origin-center' : ''}`}
-          title="Settings"
-          aria-label="Settings"
-        >
-          <svg aria-hidden="true" className="w-10 h-10" viewBox="0 0 24 24">
-            <use href="#spiderweb" />
-          </svg>
-        </button>
-      )}
-    </>
+          <use href="#spiderweb" />
+        </svg>
+      </div>
+    </button>
+  ) : (
+    <button
+      onClick={toggleSettings}
+      tabIndex={tabIndex}
+      className={`text-primary-focus cursor-pointer hover:text-primary h-full relative transition-all duration-300 ${state.showSettings ? 'text-primary rotate-180 origin-center' : ''}`}
+      title="Settings"
+      aria-label="Settings"
+    >
+      <svg aria-hidden="true" className="w-10 h-10" viewBox="0 0 24 24">
+        <use href="#spiderweb" />
+      </svg>
+    </button>
   );
 };
 
